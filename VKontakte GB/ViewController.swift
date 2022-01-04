0c696a1268a1fbe5a53fab4e6edf4b7fdf739055
ether99 <ether8@yandex.ru>
2022-01-05 00:17:26 +0300

    Initial Commit

---
+++ VKontakte GB/ViewController.swift
@@ -0,0 +1,74 @@
+//
+//  ViewController.swift
+//  VKontakte GB
+//
+//  Created by Роберт on 04.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var loginInput: UITextField!
+    @IBOutlet weak var passwordInput: UITextField!
+    
+    //    MARK: LifyCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let hideKeyBoardGesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        scrollView.addGestureRecognizer(hideKeyBoardGesture)
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyBoardWasShow),
+                                               name: UIResponder.keyboardWillShowNotification,
+                                               object: nil)
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyBoardWillBeHidden(notification:)),
+                                               name: UIResponder.keyboardWillHideNotification,
+                                               object: nil)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        NotificationCenter.default.removeObserver(self,
+                                                  name: UIResponder.keyboardWillShowNotification,
+                                                  object: nil)
+        NotificationCenter.default.removeObserver(self,
+                                                  name: UIResponder.keyboardWillHideNotification,
+                                                  object: nil)
+    }
+    
+    //    MARK: - Actions
+    @IBAction func loginButtonAction(_ sender: Any) {
+        if loginInput.text == "admin" && passwordInput.text == "1234" {
+            print("success")
+        } else {
+            print("failure")
+        }
+    }
+    
+    @objc func hideKeyboard() {
+        scrollView.endEditing(true)
+    }
+    @objc func keyBoardWasShow(notification: Notification) {
+        let info = notification.userInfo! as NSDictionary
+        let kbSize = (info.value(forKey: UIResponder.keyboardFrameEndUserInfoKey) as! NSValue).cgRectValue.size
+        let contentInset = UIEdgeInsets(top: 0, left: 0, bottom: kbSize.height, right: 0)
+        
+        self.scrollView.contentInset = contentInset
+        self.scrollView.scrollIndicatorInsets = contentInset
+    }
+    
+    @objc func keyBoardWillBeHidden(notification: Notification) {
+        
+        let contentInset = UIEdgeInsets.zero
+        scrollView.contentInset = contentInset
+    }
+}
+
